df7bd1cf36201bee45770bda476bee43a930cc65
d3mah-24 <ahmedshitech2011@gmail.com>
2023-06-09 22:07:31 +0300

    image uploaded and model created

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Achivement(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=5150)
+    photo = models.ImageField(upload_to="Achivement_img")
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    link = models.CharField(max_length=150)
+    description = models.CharField(max_length=5150)
+    photo = models.ImageField(upload_to="Project_img")
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Testimonials(models.Model):
+    name = models.CharField(max_length=150)
+    user_title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=5150)
+    photo = models.ImageField(upload_to="Testimonials_img")
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Blog(models.Model):
+    date = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=5150)
+    photo = models.ImageField(upload_to="Project_img")
+
+    def __str__(self):
+        return str(self.title)
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "app"
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'assets/'
 STATICFILES_DIRS = [
-    BASE_DIR / "assets", 
+    BASE_DIR / "assets",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
